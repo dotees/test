d2cf38ba2b9bfdd0bc3a83feda47b6fdb5d3c298
DESKTOP-JJJBO72\ASUS <ditototo24@gmail.com>
2016-10-20 08:29:39 +0700

    tolong dong, matapancing

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+	public static void main (String[] args){
+		String bata = "[==]";
+			for (int u=0; u < 10; u++) {
+				for(int i=0; i < 10; i++){
+			System.out.print(bata);
+				}
+			System.out.println(bata);
+		}
+	}
+}
